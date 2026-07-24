aff161fed3c2e4704747badd8e1497690415cdb7
Raí <rai.andrade-iconit@madeiramadeira.com.br>
2026-07-24 18:03:34 -0300

    fix: remoção de import desnecessário

--- src/test/java/services/usuarios/tests/PutUsuariosByIdTest.java
+++ src/test/java/services/usuarios/tests/PutUsuariosByIdTest.java
@@ -3,7 +3,6 @@ package services.usuarios.tests;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import config.Hooks;
